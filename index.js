aec010c563f355097f6062cf8bac39863d045303
Tincho <quintana.martin1988@gmail.com>
2019-10-17 19:30:19 -0300

    fix status code

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ fetch('https://api.imgflip.com/get_memes').then((response) => {
 	if (response.ok) {
 		console.log('Successful request!');
 	} else {
-		console.log(`Oops, we get a ${STATUS_CODE} error`);
+		console.log(`Oops, we get a ${response.status} error`);
 	}
 });
