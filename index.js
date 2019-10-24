aa3ce21fc16ba7d999704a02fc5232ee6d6e2284
Martin Quintana <m.quintana@buenosaires.gob.ar>
2019-10-24 09:46:41 -0300

    pequeño avance

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 fetch('https://api.imgflip.com/get_memes').then((response) => {
-	if (response.ok) {
-		console.log('Successful request!');
-	} else {
-		console.log(`Oops, we get a ${response.status} error`);
-	}
-});
+		if (response.ok) {
+			console.log('Successful request!');
+		} else {
+			console.log(`Oops, we get a ${response.status} error`);
+		}
+		return response;
+	})
+	.then(data => console.log(data.json()));
