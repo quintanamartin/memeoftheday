aa12e410aeda400c2880741a732a434442a8351c
Martin Quintana <m.quintana@buenosaires.gob.ar>
2019-10-30 11:20:28 -0300

    andan por separado -.-

--- index.js
+++ index.js
@@ -4,27 +4,31 @@ import { formatMemes, filterBySize, sortAscendant } from './utils.js';
 const main = document.getElementsByTagName('main');
 const memeImage = document.querySelector('.meme');
 const buttonMeme = document.querySelector('.get-meme');
-
-const memesArr = getMemes(ENDPOINT)
-  .then(response => filterBySize(response, 500))
-  .then(result => result.map(meme => formatMemes(meme)))
-  .then(formattedMemes => sortAscendant(formattedMemes));
-
-memesArr.then(response => {
-  console.dir(response);
-});
+let memesArr;
 
 function getMemeOfTheDay(memes) {
   const currentDate = new Date().getDate();
-  const memeURL = memes[currentDate - 1].url;
+  const memeURL = memes[currentDate].url;
   memeImage.setAttribute('src', `${memeURL}`);
 }
 
 function getRandomMeme(memes) {
   const randomNumber = Math.floor(Math.random() * memes.length);
+
   const memeAlt = memes[randomNumber].name;
   const memeUrl = memes[randomNumber].url;
   memeImage.setAttribute('src', `${memeUrl}`);
   memeImage.setAttribute('alt', `${memeAlt}`);
-  return memes[randomNumber];
+  return memeImage;
 }
+
+getMemes(ENDPOINT)
+  .then(response => filterBySize(response, 500))
+  .then(result => result.map(meme => formatMemes(meme)))
+  .then(formattedMemes => sortAscendant(formattedMemes))
+  .then(sortedMemes => {
+    console.dir(sortedMemes);
+    memesArr = sortedMemes;
+    //getMemeOfTheDay(memesArr);
+    buttonMeme.addEventListener('click', getRandomMeme(memesArr));
+  });
